6c57d8d6d70ece6b3dff2ed9f4a0965835cb5995
himaniv14 <113222094+himaniv14@users.noreply.github.com>
2022-09-17 00:44:57 -0700

    Added Himani's About me page

---
+++ application/client/src/Himani.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import icon from './img/himani.jpg';
+
+
+
+const ContactUs = () => {
+return (
+	<div>
+	<p class="text-center"> <p class= "h1">Himani Varshney</p></p>
+	<img src={icon} alt="icon" class= "rounded-circle"/>
+	<br></br>
+	<p class="h5"> I am Himani Varshney. I am a CS graduate student at SFSU. I am leading the team 3 for Software Engineering Project. I am a backend developer with preferred language as Python.   I have 4 years of industy experience with companies like Rolls-Royce and IBM. I am passionate about developing AI based products. On the personal front, I like swimming and playing badminton. </p>
+	
+	</div>
+);
+};
+
+export default ContactUs;
