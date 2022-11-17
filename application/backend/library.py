80f321d895a5e3fe527868f45b20455f1a2d244f
himaniv14 <113222094+himaniv14@users.noreply.github.com>
2022-11-16 16:21:34 -0800

    Added backend code for milestone 3

--- application/backend/library.py
+++ application/backend/library.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 CORS(app)
 
 # setting configuration to connect to the DB
-app.config['MYSQL_DATABASE_USER'] = 'db_admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'csc648dbpassword'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'mediastore'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
@@ -27,15 +27,49 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
-@app.route('/')
-def home():
-    print("Here")
-    return render_template('home.html')
 
 
 @app.route('/donnovan')
 def donnovan():
     return render_template('donnovan.html')
+
+#endpoint for search dropdown
+@app.route('/categories', methods=['GET', 'POST'])
+def setCategory():
+    if request.method == 'GET':
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT distinct(item_category) FROM item")
+        conn.commit()
+        category = cursor.fetchall()
+        cat_array = []
+        for cat in category:
+            cat_array.append(cat[0])
+
+        j = {"cat": cat_array}
+
+        print("j == ", j)
+        cursor.close()
+        conn.close()
+        return Response(json.dumps(cat_array),  mimetype='application/json') 
+
+#endpoint for populating items in home page
+@app.route('/home', methods=['GET'])
+def home():
+    if request.method == 'GET':
+        conn = mysql.connect()
+        cursor = conn.cursor()        
+        cursor.execute("SELECT item_title,item_description, item_author, item_path, item_price, item_category FROM item LIMIT 8")
+        conn.commit()
+        top_eight = cursor.fetchall()
+        print(top_eight)
+        jsn=[]
+        for top in top_eight:
+            jsn.append({"title": top[0], "description": top[1], "author": top[2], "path": str(top[3]), "price": top[4], "category": top[5] })
+        cursor.close()
+        conn.close()
+        return Response(json.dumps(jsn), mimetype='application/json')
+
 
 #endpoint for search
 @app.route('/search', methods=['GET', 'POST'])
@@ -53,7 +87,8 @@ def search():
         print(request.get_json())
         print(book['book'])
         print("########################")
-        
+        conn = mysql.connect()
+        cursor = conn.cursor()
         if(book['book']=="" and book['Category'] == 'all' ):
             cursor.execute("SELECT item_title,item_description, item_author, item_path, item_price, item_category FROM item")
             conn.commit()
@@ -66,6 +101,8 @@ def search():
                 jsn.append({"title": tuple_item[0], "description": tuple_item[1], "author": tuple_item[2], "path": str(tuple_item[3]), "price": tuple_item[4], "category": tuple_item[5] })
             #jsn = [ {"title": data[0][1], "description": data[0][2], "author": data[0][3], "path": str(data[0][4]), "price": data[0][5], "category": data[0][6] } ]     
             print(jsn)
+            cursor.close()
+            conn.close()
             return Response(json.dumps(jsn),  mimetype='application/json')  
         
         elif(book['Category'] == 'all' and book['book']!=""):
@@ -79,6 +116,8 @@ def search():
                 jsn.append({"title": tuple_item[0], "description": tuple_item[1], "author": tuple_item[2], "path": str(tuple_item[3]), "price": tuple_item[4], "category": tuple_item[5] })
             #jsn = [ {"title": data[0][1], "description": data[0][2], "author": data[0][3], "path": str(data[0][4]), "price": data[0][5], "category": data[0][6] } ]     
             print(jsn)
+            cursor.close()
+            conn.close()            
             return Response(json.dumps(jsn),  mimetype='application/json')  
        
         elif (book['Category'] != 'all' and book['book']==""):
@@ -92,6 +131,8 @@ def search():
                 jsn.append({"title": tuple_item[0], "description": tuple_item[1], "author": tuple_item[2], "path": str(tuple_item[3]), "price": tuple_item[4], "category": tuple_item[5] })
             #jsn = [ {"title": data[0][1], "description": data[0][2], "author": data[0][3], "path": str(data[0][4]), "price": data[0][5], "category": data[0][6] } ]     
             print(jsn)
+            cursor.close()
+            conn.close()            
             return Response(json.dumps(jsn),  mimetype='application/json')  
 
         
@@ -106,6 +147,8 @@ def search():
                 jsn.append({"title": tuple_item[0], "description": tuple_item[1], "author": tuple_item[2], "path": str(tuple_item[3]), "price": tuple_item[4], "category": tuple_item[5] })
             #jsn = [ {"title": data[0][1], "description": data[0][2], "author": data[0][3], "path": str(data[0][4]), "price": data[0][5], "category": data[0][6] } ]     
             print(jsn)
+            cursor.close()
+            conn.close()
             return Response(json.dumps(jsn),  mimetype='application/json')  
         
 
@@ -121,7 +164,8 @@ def search():
                 jsn.append({"title": tuple_item[0], "description": tuple_item[1], "author": tuple_item[2], "path": str(tuple_item[3]), "price": tuple_item[4], "category": tuple_item[5] })
             #jsn = [ {"title": data[0][1], "description": data[0][2], "author": data[0][3], "path": str(data[0][4]), "price": data[0][5], "category": data[0][6] } ]     
         print(jsn)    
-        
+        cursor.close()
+        conn.close()        
       
         return Response(json.dumps(jsn),  mimetype='application/json')
 
