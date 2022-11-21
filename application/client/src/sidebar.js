e42402575ea8e0868f17bf1d54dbbf7f6e765dea
yasi98 <ypakdel@ucdavis.edu>
2022-11-20 18:07:48 -0800

    cards size updated

--- application/client/src/sidebar.js
+++ application/client/src/sidebar.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom';
 function Sidebar(){
     return(
-        <div className='card'>
+        <div className='card' >
             <div className='list-group list-group-flush'>
                 <Link to='/userprofile' className='list-group-item list-group-item-action'> My Profile</Link>
                 <Link to='/myposts' className='list-group-item list-group-item-action'> My Posts</Link>
