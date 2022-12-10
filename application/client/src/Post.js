ee3efb18233c01dd40cbad56759ac9cd1a1ca2e7
SwansonSays <rpswanson18@gmail.com>
2022-12-09 17:06:13 -0800

    navigates to freepost or contactpost with post details depending on price

--- application/client/src/Post.js
+++ application/client/src/Post.js
@@ -8,21 +8,26 @@ import { useNavigate } from "react-router-dom";
 const Post = ({ post }) => {
     const navigate = useNavigate();
 
+    //Navigates to free post or contact post with post details
+    //depending if the post.price is free or not 
     function handleClick() {
-        if (isFree) {
+        if (isFree()) {
             navigate('/FreePost', { state: post });
         } else {
             navigate('/ContactPost', { state: post });
         }
     }
 
+    //Returns true if post is free and false if not
     function isFree() {
         if (post.price === "0.00") {
             console.log(post.price);
             return true;
         } else if (post.price === "0") {
+            console.log(post.price);
             return true;
         } else {
+            console.log(post.price);
             return false;
         }
     }
@@ -34,6 +39,7 @@ const Post = ({ post }) => {
                 <h5 className="card-title">{post.title }</h5>
                 <p className="card-text text-truncate">{post.description}</p>
                 <p className="card-text">Seller: {post.author}</p>
+                {/* Calls is free and renders as Free instead of 0.00 if returned true */}
                 <p className="card-text">Price: { isFree() ? 'Free' :  "$" + post.price}</p>
                 <div className="card-button-wrapper">
                     <button className="btn btn-secondary" onClick={handleClick}>Details</button>
